d222a1acd93c2d74e932b96a57960a745226d053
TBSDrJ <marcus.jaiclin@bishops.com>
2021-02-03 22:14:09 -0800

    Set up __init__ for Net class.

--- first_nn.py
+++ first_nn.py
@@ -42,4 +42,29 @@ print('[INFO] Done importing packages.')
 print('[INFO] Done importing packages.', file = fout, flush = True)
 
 class Net():
-    
+    def __init__(self, input_shape):
+        # input_shape is assumed to be 4 dimensions: 1. Batch Size, 2. Image Width, 3. Image Height, 4. Number of Channels.
+        # You might see this called "channels_last" format.
+        self.model = models.Sequential()
+        # For the first convolution, you need to give it the input_shape.  Notice that we chop off the batch size in the function.
+        # In our example, input_shape is 4 x 32 x 32 x 3.  But then it becomes 32 x 32 x 3, since we've chopped off the batch size.
+        # For Conv2D, you give it: Outgoing Layers, Frame size.  Everything else needs a keyword.
+        # Popular keyword choices: strides (default is strides=1), padding (="valid" means 0, ="same" means whatever gives same output width/height as input).  Not sure yet what to do if you want some other padding.
+        # Activation function is built right into the Conv2D function as a keyword argument.
+        self.model.add(layers.Conv2D(6, 5, input_shape = input_shape[1:], activation = 'relu'))
+        # In our example, output from first Conv2D is 28 x 28 x 6.
+        # For MaxPooling2D, default strides is equal to pool_size.  Batch and layers are assumed to match whatever comes in.
+        self.model.add(layers.MaxPooling2D(pool_size = 2))
+        # In our example, we are now at 14 x 14 x 6.
+        self.model.add(layers.Conv2D(16, 5, activation = 'relu'))
+        # In our example, we are now at 10 x 10 x 16.
+        self.model.add(layers.MaxPooling2D(pool_size = 2))
+        # In our example, we are now at 5 x 5 x 16.
+        self.model.add(layers.Flatten())
+        # Now, we flatten to one dimension, so we go to just length 400.
+        self.model.add(layers.Dense(120, activation = 'relu'))
+        # Now we're at length 120.
+        self.model.add(layers.Dense(84, activation = 'relu'))
+        # Now we're at length 84.
+        self.model.add(layers.Dense(10))
+        # Now we're at length 10, which is our number of classes.
