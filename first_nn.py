0ec190c709215aeb76eeb81ec409a473c85c310a
TBSDrJ <marcus.jaiclin@bishops.com>
2021-02-03 20:50:57 -0800

    Initial commit, imports.

---
+++ first_nn.py
@@ -0,0 +1,13 @@
+# Python 3.8.6
+import time
+
+# tensorflow 2.4.0
+# matplotlib 3.3.3
+# numpy 1.19.4
+# opencv-python 4.4.0
+import tensorflow.keras.models as models
+import tensorflow.keras.layers as layers
+import tensorflow.keras.backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
