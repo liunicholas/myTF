3bf5bc92a74e406954838e181fd6f409cc2b283f
TBSDrJ <marcus.jaiclin@bishops.com>
2021-02-03 21:16:04 -0800

    Set up output and constants.

--- first_nn.py
+++ first_nn.py
@@ -13,3 +13,19 @@ import sklearn.preprocessing as preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+
+fout = open('test.txt', 'w')
+now = time.strftime("%H:%M:%S", time.localtime())
+print("[TIMER] Process Time:", now)
+print("[TIMER] Process Time:", now, file = fout, flush = True)
+
+# File location to save to or load from
+MODEL_SAVE_PATH = './cifar_net.pth'
+# Set to zero to use above saved model
+TRAIN_EPOCHS = 20
+# If you want to save the model at every epoch in a subfolder set to 'True'
+SAVE_EPOCHS = False
+# If you just want to save the final output in current folder, set to 'True'
+SAVE_LAST = False
+BATCH_SIZE_TRAIN = 4
+BATCH_SIZE_TEST = 4
