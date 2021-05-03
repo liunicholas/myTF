0a3c78e33fd20513ed97be8a5190e1f90ced4e67
Nicholas Liu <nicholasliu2015@gmail.com>
2021-05-03 09:27:15 -0700

    boston housing data set features

--- myTF.py
+++ myTF.py
@@ -129,6 +129,22 @@ print("[INFO] Loading Traning and Test Datasets.")
 #get the boston housing training set
 #test_split determiones how much of the data set to be test, and seed is a random number to randomize
 ((trainX, trainY), (testX, testY)) = datasets.boston_housing.load_data(test_split=0.2, seed=113)
+
+# CRIM: Per capita crime rate by town
+# ZN: Proportion of residential land zoned for lots over 25,000 sq. ft
+# INDUS: Proportion of non-retail business acres per town
+# CHAS: Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+# NOX: Nitric oxide concentration (parts per 10 million)
+# RM: Average number of rooms per dwelling
+# AGE: Proportion of owner-occupied units built prior to 1940
+# DIS: Weighted distances to five Boston employment centers
+# RAD: Index of accessibility to radial highways
+# TAX: Full-value property tax rate per $10,000
+# PTRATIO: Pupil-teacher ratio by town
+# B: 1000(Bk — 0.63)², where Bk is the proportion of [people of African American descent] by town
+# LSTAT: Percentage of lower status of the population
+# MEDV: Median value of owner-occupied homes in $1000s
+
 # Convert from integers 0-255 to decimals 0-1.
 # trainX = trainX.astype("float") / 255.0
 # testX = testX.astype("float") / 255.0
